d7f562db374d3f574172adf9844ac7e496ff4f05
ThomasROjas15 <thdrojas@gmail.com>
2024-05-29 09:44:49 -0300

    Juego Java
    Juego Java

---
+++ ricci-rojas-ramirez-trimboli-tp-p1/src/juego/Fondojuego.java
@@ -0,0 +1,28 @@
+package juego;
+
+import java.awt.Image;
+
+import entorno.Entorno;
+import entorno.Herramientas;
+
+public class Fondojuego {
+	private int x;
+	private int y;
+	Image imagenGameOver;
+	Entorno entorno;
+	public Fondojuego() {
+
+		this.x = 400;
+		this.y = 300;
+		
+		imagenGameOver = Herramientas.cargarImagen("lavafondo.jpg");
+
+	}
+
+	public void dibujarFondo(Entorno entorno) {
+
+		entorno.dibujarImagen(imagenGameOver, this.x, this.y, 0, 1.8);
+
+	}
+
+}
